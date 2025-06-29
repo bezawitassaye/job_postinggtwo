ec196eb3e318eb97e178fdf1d94a7293d80436ea
bezawiassaye <beza1221love@gmail.com>
2025-06-29 23:04:53 +0300

    update

--- back-end/server.js
+++ back-end/server.js
@@ -8,12 +8,12 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+
 const allowedOrigins = ['https://job-postinggtwooow.onrender.com'];
 
 app.use(cors({
   origin: function(origin, callback) {
-    // Allow requests with no origin (like Postman, curl)
-    if (!origin) return callback(null, true);
+    if (!origin) return callback(null, true); // allow non-browser requests
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
