af562bbfb41ad0269b5f45df8c07df13c8564bb4
bezawiassaye <beza1221love@gmail.com>
2025-06-30 03:19:22 +0300

    update

--- front-end/pages/jobs/[id].js
+++ front-end/pages/jobs/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import Navbar from '../../components/Navbar';
+// import Navbar from '../../components/Navbar';
 
 export default function JobDetail() {
   const { query } = useRouter();
