ca29281189daa4c29e86642a9fcaff1aeaf9c607
bezawiassaye <beza1221love@gmail.com>
2025-06-30 09:26:05 +0300

    update

--- front-end/components/RegisterForm.js
+++ front-end/components/RegisterForm.js
@@ -38,39 +38,42 @@ export default function RegisterForm({ onSuccess }) {
   };
 
   return (
-    <div className='text-black'>
-      <h1 className="text-2xl mb-4">Register</h1>
-      {error && <div className="text-red-500 mb-2">{error}</div>}
+    <div className="bg-white  max-w-md w-full p-8 ">
+        <h1 className="text-3xl font-bold text-center text-sky-900 mb-6">Sign up</h1>
+       {error && <div className="text-red-500 mb-2">{error}</div>}
       {success && <div className="text-green-500 mb-2">{success}</div>}
       <form onSubmit={handleSubmit} className="space-y-4">
+        <label className="block text-gray-700 mb-1">Username</label>
         <input
           type="text"
           placeholder="Username"
           value={username}
           required
           onChange={(e) => setUsername(e.target.value)}
-          className="w-full border p-2 rounded placeholder:text-gray-500"
+          className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-sky-600 placeholder-gray-400"
         />
+        <label className="block text-gray-700 mb-1">Email</label>
         <input
           type="email"
           placeholder="Email"
           value={email}
           required
           onChange={(e) => setEmail(e.target.value)}
-          className="w-full border p-2 rounded placeholder:text-gray-500"
-        />
+        className="w-full bg-sky-900 text-white font-semibold px-4 py-3 rounded-full hover:bg-sky-800 transition"
+         />
+        <label className="block text-gray-700 mb-1">Password</label>
         <input
           type="password"
           placeholder="Password"
           value={password}
           required
           onChange={(e) => setPassword(e.target.value)}
-          className="w-full border p-2 rounded placeholder:text-gray-500"
+          className="w-full bg-sky-900 text-white font-semibold px-4 py-3 rounded-full hover:bg-sky-800 transition"
         />
         <button
           type="submit"
-          className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
-        >
+         className="w-full bg-sky-900 text-white font-semibold px-4 py-3 rounded-full hover:bg-sky-800 transition"
+         >
           Register
         </button>
       </form>
